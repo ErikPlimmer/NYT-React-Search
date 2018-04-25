5b6155cad3a2b6099a38b40a2548f9233ad766ed
Erik Plimmer <Elp013@Gmail.com>
2018-04-25 19:23:46 -0400

    renamed folder

--- react-express-NYT/client/src/App.js
+++ react-express-NYT/client/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import SearchForm from "./components/Search";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
       <strong><p>hello squritle</p></strong>
+      <SearchForm/>
       </div>
     );
   }
